eece08ad5ad3caa26a3239a122ea3a0717ce1036
Deem Ibrahim Alfaleh <dfalfaleh@gmail.com>
2024-10-20 16:36:09 +0300

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Deem Ibrahim on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
