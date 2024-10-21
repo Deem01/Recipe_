8ba448fc0a28397696dc2278d0bf57b069a2ee32
Deem Ibrahim Alfaleh <dfalfaleh@gmail.com>
2024-10-21 14:10:13 +0300

    the first commit

---
+++ Recipe/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Recipe
+//
+//  Created by Deem Ibrahim on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
