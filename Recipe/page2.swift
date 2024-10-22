64e42d6a7a39d47521454d6fcf1850d427b18b6e
Deem Ibrahim Alfaleh <dfalfaleh@gmail.com>
2024-10-22 14:18:38 +0300

    test

---
+++ Recipe/page2.swift
@@ -0,0 +1,18 @@
+//
+//  page2.swift
+//  Recipe
+//
+//  Created by Deem Ibrahim on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+struct page2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    page2()
+}
